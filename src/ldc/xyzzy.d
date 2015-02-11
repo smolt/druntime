e70a5d55bd8bc42b4e980703f2f605d80814300c
Dan Olson <danole@comcast.net>
2015-02-10 17:02:47 -0800

    Additional unittest helpers
    Stuff to make it easier to debug unittests.

--- src/ldc/xyzzy.d
+++ src/ldc/xyzzy.d
@@ -1,16 +1,84 @@
 // A temporary place to drop in some hacks and other magic while porting iOS
 module ldc.xyzzy;
 
+import core.atomic;
+import core.stdc.stdio;
+import core.sys.posix.pthread;
+
 shared uint skippedTests;
+shared uint failedTests;
 
 void skipTest()()
 {
-    pragma(msg, "Note: skipping tests with error");
-    ++skippedTests;
+    pragma(msg, "Note: some tests for this target are being skipped");
+    atomicOp!"+="(skippedTests, 1);
 }
 
+void failedTest()
+{
+    atomicOp!"+="(failedTests, 1);
+}
 
-import core.sys.posix.pthread;
+void breakpt()()
+{
+    // set an ARM breakpoint.  In debugger, set pc to next instruction to continue
+    version (ARM)
+    {
+        import ldc.llvmasm;
+        __asm("bkpt", "");
+    }
+    else
+        static assert(0, "breakpt only for ARM");
+}
+
+mixin template testhelp()
+{
+    bool showExpr(string expr)()
+    {
+        // cheat and use write to make printing of any type easier.
+        // Eventually make so not depending on phobos here.
+        import std.stdio: writefln;
+        writefln("%s = %f", expr, mixin(expr));
+        return true;
+    }
+
+    bool testTrue(string expr, string msg = null,
+                  string file = __FILE__, size_t line = __LINE__)()
+    {
+        import core.stdc.stdio : printf;
+        import ldc.xyzzy;
+        
+        immutable r = mixin(expr);
+        if (r)
+        {
+            printf("%s(%lu): OK '%s' (Hey, this works now)\n",
+                   file.ptr, line, expr.ptr);
+        }
+        else
+        {
+            failedTest();
+            printf("%s(%lu): FAIL '%s'\n", file.ptr, line, expr.ptr);
+            if (msg)
+            {
+                printf("%s\n", msg.ptr);
+            }
+        }
+        return r;
+    }
+}
+
+// struct rep of string array for passing into printf style funcs with len,ptr
+struct cvstr
+{
+    const int len;                           // "%.*s" expects int, not size_t
+    const char *ptr;
+
+    this(string s)
+    {
+        len = cast(int)s.length;
+        ptr = s.ptr;
+    }
+}
 
 struct ThreadLocal(T)
 {
